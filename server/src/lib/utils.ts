a5a932dd70741341cc542f6ebcb342f1ea08e954
benjoquilario <benjoquilario@gmail.com>
2024-11-22 20:51:56 +0800

    feat: verifyJwt token

---
+++ server/src/lib/utils.ts
@@ -0,0 +1,16 @@
+import { compare, hash } from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+const SALT_ROUNDS = 10;
+export async function comparePassword(password: string, hash: string) {
+  return compare(password, hash);
+}
+
+export async function hashPassword(password: string) {
+  return hash(password, SALT_ROUNDS);
+}
+//
+
+export function verifyJwt(token: string) {
+  return jwt.verify(token, process.env.AUTH_SECRET!);
+}
